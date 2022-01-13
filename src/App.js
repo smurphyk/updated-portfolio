4af41ca6ff710c45a0cd5d57686fe3bf607fa961
smurphyk <sean.murphy131@gmail.com>
2022-01-13 16:06:52 -0500

    began creating header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Header from './components/Header'
+
 const App = () => {
-  return <div>Sean Murphy</div>
+  return (
+    <div className='flex items-center justify-center h-screen'>
+      <Header />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import logo from './logo.jpg'
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} alt='logo' width={75} />
+    </header>
+  )
+}
+
+export default Header
