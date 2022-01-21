048ceaa7a2567c4f6f4d9d80d6807d005c7ee3cd
smurphyk <sean.murphy131@gmail.com>
2022-01-21 16:02:23 -0500

    updated header styling

--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -6,7 +6,7 @@ const Navigation = () => {
     <div className='flex flex-col'>
       {links.map((link) => (
         <ul key={link}>
-          <li className='py-10 text-2xl text-teal-dark dark:text-teal-light'>
+          <li className='py-10 text-2xl text-teal-light dark:text-teal-dark'>
             {link}
           </li>
         </ul>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,10 +5,15 @@ import Navigation from './Navigation'
 
 const Header = () => {
   return (
-    <header className='flex flex-col items-start dark:border-white p-14'>
+    <header className='flex flex-col items-start bg-grey-light p-14 h-screen w-1/5 border-r-2 border-teal-dark dark:border-teal-light'>
       <div className='flex flex-col items-center mb-10'>
-        <img src={logo} alt='logo' width={75} />
-        <p className='font-bold text-3xl text-teal-dark dark:text-teal-light'>
+        <img
+          src={logo}
+          alt='logo'
+          width={75}
+          className='border-2 rounded-full border-teal-dark'
+        />
+        <p className='font-bold mt-5 text-3xl text-teal-light dark:text-teal-dark'>
           Sean Murphy
         </p>
       </div>
